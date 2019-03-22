e40fddaa807d3a1c818825d5b0e5e5b4ec3ad202
Richa Gaur <richagaur507@gmail.com>
2019-03-22 15:34:15 +0530

    angular application initialized

--- src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const HOME = '/home';
-export const CHATROOM = '/chat/:id';
-export const home = '/home/:id';
-export const landing = '/';
